895f3f5c7a7fe5e4c066646929ad26fe9ac728e8
Oluwatimi11 <57862887+Oluwatimi11@users.noreply.github.com>
2021-11-08 14:17:21 +0100

    Hello-World-2, React Basics

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
+import PureComp from "./components/PureComp";
+import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
+import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
+import ClickCounterTwo from "./components/ClickCounterTwo";
+import HoverCounterTwo from "./components/HoverCounterTwo";
+import User from "./components/User";
+import Counter from "./components/Counter";
+import ComponentC from "./components/ComponentC";
+import { UserProvider } from './components/UserContext'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FragmentDemo /> */}
+      {/* <Table /> */}
+      {/* <ParentComp /> */}
+      {/* <RefsDemo /> */}
+      {/* <FocusInput /> */}
+      {/* <FRParentInput /> */}
+      {/* <PortalDemo /> */}
+      {/* <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary> */}
+      {/* <ClickCounter name="Samuel"/> */}
+      {/* <HoverCounter /> */}
+      {/* <ClickCounterTwo />
+      <HoverCounterTwo />
+      <User render={ (isLoggedIn) => isLoggedIn ? "Samuel" : "Guest"} /> */}
+      {/* <Counter>
+        {(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      </Counter>
+      <Counter>
+        {(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      </Counter> */}
+      <UserProvider value="Samuel">
+        <ComponentC />
+      </UserProvider>
+    </div>
+  );
+}
+
+export default App;
